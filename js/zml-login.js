d96467ab35e7748942b7a0ee5448ae838fb69e2a
lllcsunny <871862636@qq.com>
2017-07-02 20:47:26 +0800

    z20.47

--- js/zml-login.js
+++ js/zml-login.js
@@ -2,54 +2,51 @@
  * Created by hp1 on 2017/7/2.
  */
 $().ready(function(){
-    $('.mui-input-group').validate({
-        rules:{
-            text:{
-                required:true,
-                minlength:6,
-                remote:{
-                    url:'checkname.php',
-                    type:"post",
-                    dataType:'json',
-                    data:{username:function(){
-                        return $('#account').val();
-                    }}
-                }
-            },
-            pass:{
-                required:true,
-                minlength:6
-            },
-            rpass:{
-                required: true,
-                minlength: 6,
-                equalTo: "#password"
-            },
-            email:{
-                required:true,
-                email:true
+    let flag1=true;
+    let flag2=true;
+    function getstyle(card1,card2,content){
+        $(card1).on("touchstart",function(){
+            if(flag1){
+                flag1=false;
+                // console.log($(content));
+                $(card1).eq(1).addClass('first');
+                $(card1).eq(0).removeClass('first');
+                $(card2).eq(1).addClass('first');
+                $(card2).eq(0).removeClass('first');
+                $(content).removeClass("first");
+                $(content).eq(1).addClass('first');
+                flag2=true;
             }
-        },
-        messages:{
-            text:{
-                required:'必填',
-                minlength:'用户名不能少于6个字符',
-                remote:'用户名已存在'
+        });
+        $(card2).on("touchstart",function(){
+            if(flag2){
+                flag2=false;
+                $(card1).eq(0).addClass('first');
+                $(card1).eq(1).removeClass('first');
+                $(card2).eq(0).addClass('first');
+                $(card2).eq(1).removeClass('first');
+                $(content).removeClass("first");
+                $(content).eq(0).addClass("first");
+                flag1=true;
+            }
+        });
+    }
+    getstyle(".zml-denglu div span:nth-child(1) img",".zml-denglu div span:nth-child(2) img",".zml-tianxie");
+    $("input").each(function(i){
+        $("input")[i].oninput = function() {
+            if($("input[type=text]").val()&&$("input[type=password]").val()){
+                $(".zml-login1").addClass("first");
+                $(".zml-login2").removeClass("first");
+                $(".zml-login2").click(function(){
+                    $(".zml-login2").addClass("first");
+                    $(".zml-login3").removeClass("first");
+                    setTimeout(function(){
+                        location.href="index.html";
+                    },2000);
+                })
+            }
 
-            },
-            pass:{
-                required:'用户名不能为空',
-                minlength:'密码不能少于6个字符'
-            },
-            rpass:{
-                required: '必填',
-                minlength: 6,
-                equalTo: "两次密码输入不一致"
-            },
-            email:{
-                required:'必填',
-                email:'请填写正确的email格式'
-            }
-        }
+
+        };
     });
 });
